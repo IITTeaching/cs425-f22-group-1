c4dfe397ae676b4d41badbef08bcb5fe9d8961c8
akina <111941180+akcast8@users.noreply.github.com>
2022-11-20 19:01:07 -0600

    Update DDLscript.sql

--- DDLscript.sql
+++ DDLscript.sql
@@ -1,5 +1,70 @@
-CREATE TABLE Branch();
-CREATE TABLE Employee();
-CREATE TABLE Customer();
-CREATE TABLE Account();
-CREATE TABLE Loan();
+CREATE TABLE branch(
+  branch_ID NUMERIC(9),
+  address VARCHAR(20),
+  street,
+    street_number INTEGER NOT NULL,
+    street_name VARCHAR(20),
+  city VARCHAR(20),
+  state CHAR(2),
+  zip NUMERIC(5),
+  PRIMARY KEY (branch_ID)
+);
+
+CREATE TABLE employee(
+  SSN NUMERIC(9),
+  name,
+    first_name VARCHAR(256),
+    last_name VARCHAR(256),
+  home_address,
+    street,
+      street_number INTEGER NOT NULL,
+      street_name VARCHAR(20),
+      apt_number NUMERIC(10),
+    city VARCHAR(20),
+    state CHAR(2),
+    zip NUMERIC(5)
+  employee_type VARCHAR(20),
+  salary NUMERIC(8,2),
+  PRIMARY KEY (SSN),
+  CHECK (occupation = 'teller' OR occupation = 'loan specialist' OR occupation = 'manager')
+);
+
+CREATE TABLE customer(
+  SSN NUMERIC(9),
+  name,
+    first_name VARCHAR(256),
+    last_name VARCHAR(256),
+  home_address,
+    street,
+      street_number INTEGER NOT NULL,
+      street_name VARCHAR(20),
+      apt_number NUMERIC(10),
+    city VARCHAR(20),
+    state CHAR(2),
+    zip NUMERIC(5),
+  branch VARCHAR(20),
+  PRIMARY KEY (SSN),
+  FOREIGN KEY branch references branch(branch_ID)
+);
+
+CREATE TABLE account(
+  account_num NUMERIC(12),
+  account_type VARCHAR(20),
+  balance NUMERIC(30,2),
+  PRIMARY KEY (account_num)
+);
+
+CREATE TABLE loan(
+  loan_ID NUMERIC(9),
+  account VARCHAR(20),
+  runtime INTEGER NOT NULL,
+  interest_schedule VARCHAR(256),
+  PRIMARY KEY (loan_ID)
+);
+
+CREATE TABLE transaction(
+  transaction_type CHAR(10),
+  transaction_amount NUMERIC(30,2),
+  description VARCHAR(256),
+  PRIMARY KEY (transaction_ID)
+);
